5a119d8ed2a85408fbcc4d819bf626341b660cff
gleidsondevC <gleidsonlive01@gmail.com>
2021-03-01 06:41:53 -0300

    nomeando variaveis

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function easy(element) {
 var dados,// recebe os dados da api
   imag, x,
   nome;
+  
+  var img = easy('#img'); // imagem 
+var pokename = easy('#name');// input do nome
+var Id = easy('.id');
+var pnome = easy('.pokename')
+var tipo = easy('.tipo');
+var hab = easy('.habilidades')
+var searchbut = easy('.button')
+var altura = easy('.altura')
+var peso = easy('.peso')
+var conteiner = easy('.conteiner')
