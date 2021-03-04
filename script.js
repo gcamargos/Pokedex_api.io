5cdfc566d253b2c4b9e01fcb55058a87540eebc8
gleidsondevC <gleidsonlive01@gmail.com>
2021-03-04 17:29:45 -0300

    funcão atribuitiva

--- script.js
+++ script.js
@@ -32,3 +32,16 @@ function iniciapp(nome) {
     atribui();
   }, 500);
 }
+
+function atribui() {
+  pnome.innerHTML = `${dados.name}`
+  Id.innerHTML = `Nº:${dados.id}`;
+  img.src = dados.sprites.front_default;
+  tipo.innerHTML = `TIPO: ${dados.types.map(item => ' ' + item.type.name.toString())} `; // PENDENCIA .MAP
+  hab.innerHTML = dados.moves.map(item => ' ' + item.move.name.toString())
+  var weight = dados.weight * 0.1;
+  var height = dados.height * 0.1;
+  peso.innerHTML = `PESO:${parseFloat(weight.toFixed(2))}kg`
+  altura.innerHTML = `ALTURA: ${parseFloat(height.toFixed(2))}m`
+
+}
